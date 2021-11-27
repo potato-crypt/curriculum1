5415894fb88f071e01a6447309d423de32598ed4
potato-crypt <poohsan152@gmail.com>
2021-11-27 10:17:30 +0900

    課題

--- 1-3/Task1_3.java
+++ 1-3/Task1_3.java
@@ -3,7 +3,7 @@ public class Task1_3 {
     public static void main(String[] args) {
 
         // ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
-        int plus = 5 + 7;
+        int plus = 5 + 10;
         // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
         int minus = 20 -7;
         // ③下記の処理について、何をしているのかコメントを記入してください。
@@ -11,7 +11,7 @@ public class Task1_3 {
         int multiply = 10 * 2;
 
         // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
-        int remi = 20 / 6; 
+        int remi = 20 % 6; 
         // ⑤下記の処理について、何をしているのかコメントを記入してください
         // [String型のtownとlineにそれぞれ有楽町と線という文字列を代入している。]
         String town = "有楽町";
@@ -21,6 +21,7 @@ public class Task1_3 {
         String train = town + line;
         // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
         System.out.println(plus);
+        System.out.println(minus);
         System.out.println(multiply);
         System.out.println(remi);
         System.out.println(train);
